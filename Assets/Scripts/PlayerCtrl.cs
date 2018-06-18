4d31952137a74e265eeff26ee19c88434f1578f4
Tiago <tiagooliverspid@hotmail.com>
2018-06-18 11:57:06 -0300

    Fazendo o player pular

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	public float horizontalSpeed = 10f;
+	public float jumpSpeed = 300f;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float horizontalInput = Input.GetAxisRaw("Horizontal");
+		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+		if (horizontalPlayerSpeed != 0){
+			MoveHorizontal(horizontalPlayerSpeed);
+		}
+		else {
+			StopMovingHorizontal();
+		}
+		if (Input.GetButtonDown("Jump")){
+			Jump();
+		}
+	}
+
+	void MoveHorizontal(float speed){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+	}
+
+	void StopMovingHorizontal(){
+		rb.velocity = new Vector2(0f, rb.velocity.y);
+	}
+
+	void Jump(){
+		rb.AddForce(new Vector2(0f, jumpSpeed));
+	}
+}
