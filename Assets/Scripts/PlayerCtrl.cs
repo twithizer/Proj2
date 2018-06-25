80815e221046cbd3fdddab89da9c259e3379ed2c
Tiago <tiagooliverspid@hotmail.com>
2018-06-25 10:14:06 -0300

    Colocando as animacoes

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -9,12 +9,15 @@ public class PlayerCtrl : MonoBehaviour {
 
 	Rigidbody2D rb;
 	SpriteRenderer sr;
+	Animator anim;
+
+	bool isJumping = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
-
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -43,13 +46,26 @@ public class PlayerCtrl : MonoBehaviour {
 			sr.flipX = false;
 		}
 
+		if (!isJumping){
+		anim.SetInteger("State",2);}
+
 	}
 
 	void StopMovingHorizontal(){
 		rb.velocity = new Vector2(0f, rb.velocity.y);
+		if (!isJumping){
+		anim.SetInteger("State", 0);}
 	}
 
 	void Jump(){
+		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+			isJumping = false;
+		}
 	}
 }
