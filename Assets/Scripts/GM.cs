4ecd1b7997bb221ccdd3269ae0650381834ef3ad
Tiago <tiagooliverspid@hotmail.com>
2018-08-14 09:33:30 -0300

    Botoes

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GM : MonoBehaviour {
 
@@ -49,6 +50,22 @@ public class GM : MonoBehaviour {
 		}
 		UpdateTimer();
 		DisplayHudData();
+	}
+
+	public void Restartlevel(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void ExitToMainMenu(){
+		LoadScene("MainMenu");
+	}
+
+	public void CloseApp(){
+		Application.Quit();
+	}
+
+	public void LoadScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
 	}
 	
 	void UpdateTimer(){
